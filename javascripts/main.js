89712cef386d3d81ed3dcb113dcdc4bc974c8478
daixianfeng <dai_xianfeng@hotmail.com>
2013-07-31 16:45:20 +0800

    gh-pages update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,7 @@
 (function(){
 
 $(document).ready(function(){
+	
 	$('#back-feature td').on('mouseenter',function(e){
 		var target = $(this);
 		target.find('p:first').fadeOut('fast');
@@ -10,6 +11,26 @@ $(document).ready(function(){
 		target.find('p:first').fadeIn('fast');
 		target.find('p:last').fadeOut('fast');
 	});
+	$('ul.version-list li p.version-opt span').on('mouseenter',function(e){
+		var target = $(this);
+		target.css('cursor','pointer');
+		target.css('color','#2268b2');
+	}).on('mouseleave',function(e){
+		var target = $(this);
+		target.css('color','#666');
+	}).on('click',function(e){
+		var target = $(this);
+		target.closest('li').find('div.version-info').slideToggle('fast');
+	});
 });
-
+function loadCss(url){
+	var link = document.createElement("link");
+	
+	link.setAttribute('type', 'text/css');
+	link.setAttribute('rel', 'stylesheet');
+	link.setAttribute('href', url);
+	
+	// 此处必须使用原生的DOM方法加载css文件，不能使用jQuery的append方法加载
+	$("head")[0].appendChild(link);
+};
 })()
